0e61e8400b91ed7ee249ebfb4e43dedc96f95a6f
victor <vjbsh@hotmail.com>
2023-11-17 16:54:12 -0600

    updated some settings and added a brew detector

--- fish/config.fish
+++ fish/config.fish
@@ -5,7 +5,11 @@ end
 function nvm
     bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
 end
-
+if uname -m | grep -q arm64
+    eval (/opt/homebrew/bin/brew shellenv)
+else
+    eval (/usr/local/bin/brew shellenv)
+end
 complete -f -c dotnet -a "(dotnet complete (commandline -cp))"
 starship init fish | source
 status --is-interactive; and /opt/homebrew/bin/rbenv init - fish | source
