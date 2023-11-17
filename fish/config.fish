e816fb9f3f3235a21554d330b0749d31fc33d8b6
victor <vjbsh@hotmail.com>
2023-11-17 02:21:06 -0600

    modified fish config

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,6 @@ function nvm
     bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
 end
 
-
+complete -f -c dotnet -a "(dotnet complete (commandline -cp))"
 starship init fish | source
 status --is-interactive; and /opt/homebrew/bin/rbenv init - fish | source
