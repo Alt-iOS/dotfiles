d54f9fb03385db493c018913b5675322bae32757
victor <vjbsh@hotmail.com>
2023-11-17 17:17:51 -0600

    some changes

--- fish/config.fish
+++ fish/config.fish
@@ -2,14 +2,17 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-function nvm
-    bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
-end
 if uname -m | grep -q arm64
     eval (/opt/homebrew/bin/brew shellenv)
 else
     eval (/usr/local/bin/brew shellenv)
 end
+
 complete -f -c dotnet -a "(dotnet complete (commandline -cp))"
+
 starship init fish | source
+
+
+load_nvm >/dev/stderr
+
 status --is-interactive; and /opt/homebrew/bin/rbenv init - fish | source
